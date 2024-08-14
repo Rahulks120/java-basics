d78307ebe302bd738efb390021e0e335880189f6
Rahulks120 <rahulk@ivoyant.com>
2024-08-14 17:43:31 +0530

    Adding Arithmeticoperator File

---
+++ src/com/ivoyant/java/ArithmeticOperators.java
@@ -0,0 +1,22 @@
+package com.ivoyant.java;
+
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+
+
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
+        int quotient = num1 / num2;
+        int remainder = num1 % num2;
+
+        System.out.println("Arithmetic Operations:");
+        System.out.println("Sum: " + num1 + " + " + num2 + " = " + sum);
+        System.out.println("Difference: " + num1 + " - " + num2 + " = " + difference);
+        System.out.println("Product: " + num1 + " * " + num2 + " = " + product);
+        System.out.println("Quotient: " + num1 + " / " + num2 + " = " + quotient);
+        System.out.println("Remainder: " + num1 + " % " + num2 + " = " + remainder);
+    }
+}
