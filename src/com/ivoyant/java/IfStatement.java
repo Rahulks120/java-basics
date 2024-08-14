33ce50e6fc4e8e72ba23191ee7886699d90fdaec
Rahulks120 <rahulk@ivoyant.com>
2024-08-14 21:59:44 +0530

    IfStatementDemo

--- src/com/ivoyant/java/IfStatement.java
+++ src/com/ivoyant/java/IfStatement.java
@@ -2,7 +2,7 @@ package com.ivoyant.java;
 
 import java.util.Scanner;
 
-public class IfStmt {
+public class IfStatement {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         int val =sc.nextInt();
