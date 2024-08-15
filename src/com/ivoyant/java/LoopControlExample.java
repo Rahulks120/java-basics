408deb2f24bbdc8223ad91d943d5baf1513c1bab
Rahulks120 <rahulk@ivoyant.com>
2024-08-15 12:13:59 +0530

    Update LoopControlExample

--- src/com/ivoyant/java/LoopControlExample.java
+++ src/com/ivoyant/java/LoopControlExample.java
@@ -5,21 +5,15 @@ public class LoopControlExample {
     public static void main(String[] args) {
        
         for (int i = 0; i <= 10; i++) {
-
-            
             if (i == 5) {
                 continue;  
             }
-
-   
             System.out.println("i = " + i);
-
          
             if (i == 8) {
                 break;  
             }
         }
-
         System.out.println("Loop exited.");
     }
 }
