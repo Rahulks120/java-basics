4d3d280001946ca8c964f982194997d5d6185ad0
Rahulks120 <rahulk@ivoyant.com>
2024-08-19 13:01:48 +0530

    serialize

--- src/com/ivoyant/java/SerializeDemo/Person.java
+++ src/com/ivoyant/java/SerializeDemo/Person.java
@@ -4,8 +4,12 @@ import java.io.Serializable;
 
 public class Person implements Serializable {
 
+    private  static final long serialVersionUID = 7581288912016876506L;
+
     private String name;
     private int age;
+
+    private String gender="male";
 
     public Person(String name, int age) {
         this.name = name;
---
+++ src/com/ivoyant/java/threads/ThreadsRunnable.java
@@ -0,0 +1,2 @@
+package com.ivoyant.java.threads;public class ThreadsRunnable {
+}
